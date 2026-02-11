31d3d9cb06797c84cb1a8e7cbdb98d37f9a207d2
Pedro Freire <pedropfpj@gmail.com>
2026-02-11 12:08:04 -0300

    Repositório Sprint 7 TripleTen

--- data.py
+++ data.py
@@ -1,1 +1,7 @@
-# Arquivo com constantes
+URBAN_ROUTES_URL = 'https://cnt-ddc9693e-ad4f-4e87-9f32-38407dd0a9ce.containerhub.tripleten-services.com/?lng=pt'
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE =  '1111'
+MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
